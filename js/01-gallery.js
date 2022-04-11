1e07896bb1a3df90d2931bfd501ceb739dab852f
ShenyaLipinsky <shenyalipinskiy@gmail.com>
2022-04-11 16:15:59 +0300

    FirstEnd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,20 +2,39 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryBlock = document.querySelector("div.gallery");
-
+const lightBox = document.querySelector(".basicLightbox--visible");
 const galleryItemsCreate = galleryItems
   .map((image) => {
-    return ` <div class="gallery__item"><a  href="${image.original}" class="gallery__link"><img src="${image.preview}" alt="${image.description}" srcset="${image.original}" class="gallery__image"></a></div>`;
+    return ` <div class="gallery__item"><a  href="${image.original}" class="gallery__link"><img src="${image.preview}" alt="${image.description}" data-source="${image.original}" class="gallery__image"></a></div>`;
   })
   .join("");
 
 galleryBlock.innerHTML = galleryItemsCreate;
+galleryBlock.addEventListener("click", onImageItemClick);
 
-galleryBlock.addEventListener("click", imageItem);
-
-function imageItem(item) {
-  if (item.target.nodeName !== "a") {
+function onImageItemClick(item) {
+  if (item.target.nodeName !== "IMG") {
     return;
   }
-  console.log("akakakakakakak");
+  item.preventDefault();
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${item.target.dataset.source}">
+    `,
+    {
+      onShow: (instance) => {
+        const listener = function (event) {
+          // console.log("key: ", event.key);
+          if (event.key === "Escape") {
+            document.removeEventListener("keydown", listener);
+            return instance.close();
+          }
+        };
+        document.addEventListener("keydown", listener);
+      },
+    }
+  );
+
+  instance.show();
 }
