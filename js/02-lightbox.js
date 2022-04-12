bbc24eabb9d95873d87d2f45fdb1fd1aff4f638f
ShenyaLipinsky <shenyalipinskiy@gmail.com>
2022-04-12 12:27:30 +0300

    almost end 2nd part

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryBlock = document.querySelector("ul.gallery");
+const lightBox = document.querySelector(".basicLightbox--visible");
+const galleryItemsCreate = galleryItems
+  .map((image) => {
+    return ` <a  href="${image.original}" class="gallery__item"><img src="${image.preview}" alt="${image.description}"  class="gallery__image"></a>`;
+  })
+  .join("");
 
-console.log(galleryItems);
+galleryBlock.innerHTML = galleryItemsCreate;
+
+galleryBlock.addEventListener("click", onImageItemClick);
+
+function onImageItemClick(item) {
+  if (item.target.nodeName !== "IMG") {
+    return;
+  }
+  item.preventDefault();
+
+  const simpleLightBoxCreate = new SimpleLightbox(".gallery .gallery__item", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  if (simpleLightBoxCreate.close) {
+    simpleLightBoxCreate.destroy;
+  }
+}
